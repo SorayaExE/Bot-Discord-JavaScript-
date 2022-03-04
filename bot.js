9e2a3ee53329a01c6598c5d001e058b776ce6f2a
Júlio <juliodesoll@gmail.com>
2022-03-03 22:52:07 -0300

    make it better

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 const fetch = require('node-fetch');
 const Enmap = require("enmap");
+const db = require('quick.db');
 const fs = require("fs");
 const moment = require('moment');
 require('moment-duration-format');
@@ -102,6 +103,83 @@ client.on("message", async message => {
 
     member.send(joinEmbed);
     });
+
+    client.on("voiceStateUpdate", (oldMember, newMember) => {
+      
+      const canal_logs = db.get(`config.${oldMember.guild.id}.tempocall`);
+      
+      let usuario = newMember.guild.members.cache.get(newMember.id)
+  
+    let oldVoice = oldMember.channel; 
+    let newVoice = newMember.channel; 
+    let canal_logs2 = client.channels.cache.find(channel => channel.id == canal_logs)
+    if (oldVoice == null) {
+              let embed = new Discord.MessageEmbed()
+        .setTitle('Entrou no Canal de Voz')
+        .setDescription(`O usuário <@${newMember.id}> entrou no canal de voz ${newMember.channel}.`)
+        .setColor("RANDOM")
+        canal_logs2.send({ embeds: [embed] })
+        
+        
+          if (db.get(`contando_${newMember.id}`) === false) {
+            db.set(`contando_${newMember.id}`, true)
+        db.set(`call_${newMember.id}`, new Date().getTime())
+            db.set(`contando_${newMember.id}`, true)
+          }
+    } else if (newVoice == null) {
+            let embed = new Discord.MessageEmbed()
+        .setTitle('Saiu do Canal de Voz')
+        .setDescription(`O usuário <@${newMember.id}> saiu do canal de voz ${oldMember.channel}.`)
+        .setColor("RANDOM")
+        canal_logs2.send({ embeds: [embed] })
+        
+        
+          if (db.get(`contando_${newMember.id}`) === true) {
+        const tempo =  db.get(`call_${newMember.id}`)
+        
+        const start = new Date().getTime();
+       
+        const diff = Math.abs(tempo - start);
+        
+        const tempo2 = Math.ceil(diff / 1000)
+  
+        db.add(`tempocall_${newMember.id}`, tempo2)
+            db.set(`contando_${newMember.id}`, false)
+        }
+    } else {
+        let embed = new Discord.MessageEmbed()
+        .setTitle('Mudou de Canal de Voz')
+        .setDescription(`O usuário <@${newMember.id}> mudou de canal de voz.`)
+        .addField('Saiu de', `${oldMember.channel}.`, true)
+        .addField('Entrou em', `${newMember.channel}.`, true)
+        .setColor("RANDOM")
+        canal_logs2.send({ embeds: [embed] })
+  
+    }
+        if (newMember.selfMute === true) {
+          if (usuario.voice.channel) {
+              if (db.get(`contando_${newMember.id}`) === true) {
+                const tempo =  db.get(`call_${newMember.id}`) 
+            const start = new Date().getTime(); 
+       
+            const diff = Math.abs(tempo - start);
+            const tempo2 = Math.ceil(diff / 1000)
+  
+            db.add(`tempocall_${newMember.id}`, tempo2)
+                db.set(`contando_${newMember.id}`, false)
+                return;
+              } else {
+                  return;
+              }
+         }
+       } else {
+           if (usuario.voice.channel) {
+             db.set(`call_${newMember.id}`, new Date().getTime())
+             db.set(`contando_${newMember.id}`, true)
+         }
+         return;
+        }
+    })
 
 client.login(config.token);
 
