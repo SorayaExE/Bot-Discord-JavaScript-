7b9105f8d6697e03d681a965bd993bee9a6d739a
Júlio <juliodesoll@gmail.com>
2022-03-07 14:18:21 -0300

    make it better

--- commands/aviso4.js
+++ commands/aviso4.js
@@ -21,8 +21,8 @@ const embed = new Discord.MessageEmbed()
 .setDescription('Benefícios para vips no Desocupados:')
 .setAuthor(`${desocupado.username}`, desocupado.avatarURL())
 .addFields(
-  { name: '<a:Y_cafe:749473046074622023>𝐃𝐞𝐬𝐨𝐜𝐮𝐩𝐚𝐝𝐨', value: `Permissão para enviar imagens no chat<#711373689504923708> \n200k na economia \nTag exclusiva (Desocupado) \nSala Vip` },
-  { name: '<a:L_bongo_cat:949191419447357530>𝐃𝐞𝐬𝐨𝐜𝐮𝐩𝐚𝐝𝐨 𝐩𝐥𝐮𝐬 ', value: `Permissão para enviar imagens no chat<#711373689504923708> \n500k na economia \nTag exclusiva (Desocupado Plus) \nSala vip \nNitro classic (1 mês)` },
+  { name: '<a:Y_cafe:749473046074622023>𝐃𝐞𝐬𝐨𝐜𝐮𝐩𝐚𝐝𝐨', value: `Permissão para enviar imagens no chat<#949751951602647110> \n200k na economia \nTag exclusiva (Desocupado) \nSala Vip` },
+  { name: '<a:L_bongo_cat:949191419447357530>𝐃𝐞𝐬𝐨𝐜𝐮𝐩𝐚𝐝𝐨 𝐩𝐥𝐮𝐬 ', value: `Permissão para enviar imagens no chat<#949751951602647110> \n500k na economia \nTag exclusiva (Desocupado Plus) \nSala vip \nNitro classic (1 mês)` },
   { name: '<a:Y_Nezuko_jump:949186370834956289>𝐃𝐞𝐬𝐨𝐜𝐮𝐩𝐚𝐝𝐨 𝐦𝐚𝐠𝐧𝐚𝐭𝐚', value: `Sala e call exclusiva para você e seus amigos \nSorteios vips \nPermissão para adicionar emojis \nDiscord Nitro (1 mês) \n1kk na economia \nTag exclusiva (Desocupado Magnata)` }
 )
 .setTimestamp()
